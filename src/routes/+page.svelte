67cbf1d44c0593529578abb88882d32d1b4c5eaa
roy.crippen4 <roy.crippen4@archarithms.com>
2024-11-17 18:22:17 -0500

    simple working example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,155 +1,39 @@
 <script lang="ts">
-  import { invoke } from "@tauri-apps/api/core";
+  import { Ion, Viewer } from 'cesium';
+  import 'cesium/Build/Cesium/Widgets/widgets.css';
+  import { onMount } from 'svelte';
+  import { invoke } from '@tauri-apps/api/core';
 
-  let name = $state("");
-  let greetMsg = $state("");
+  Ion.defaultAccessToken =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI5NjNmM2ZjNS0yMjhhLTRiNGMtYjNkZS1hMzA4NGViMTRlZTYiLCJpZCI6MTY3OTExLCJpYXQiOjE2OTUzNjIxMjZ9.m9st8lwGsSaP6ZjXUKVomVHVu-liiEskl4nLbQoj_KQ';
+
+  let viewer: Viewer;
+  onMount(() => {
+    viewer = new Viewer('cesiumContainer', {
+      animation: false,
+      homeButton: false,
+      timeline: false,
+      fullscreenButton: false,
+    });
+  });
+
+  let name = $state('');
+  let greetMsg = $state('');
 
   async function greet(event: Event) {
     event.preventDefault();
-    greetMsg = await invoke("greet", { name });
+    greetMsg = await invoke('greet', { name });
   }
 </script>
 
-<main class="container">
-  <h1>Welcome to Tauri + Svelte</h1>
-
-  <div class="row">
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
-    </a>
-    <a href="https://tauri.app" target="_blank">
-      <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
-    </a>
-    <a href="https://kit.svelte.dev" target="_blank">
-      <img src="/svelte.svg" class="logo svelte-kit" alt="SvelteKit Logo" />
-    </a>
-  </div>
-  <p>Click on the Tauri, Vite, and SvelteKit logos to learn more.</p>
-
-  <form class="row" onsubmit={greet}>
-    <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
-    <button type="submit">Greet</button>
-  </form>
-  <p>{greetMsg}</p>
-</main>
-
-<style>
-.logo.vite:hover {
-  filter: drop-shadow(0 0 2em #747bff);
-}
-
-.logo.svelte-kit:hover {
-  filter: drop-shadow(0 0 2em #ff3e00);
-}
-
-:root {
-  font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
-  font-size: 16px;
-  line-height: 24px;
-  font-weight: 400;
-
-  color: #0f0f0f;
-  background-color: #f6f6f6;
-
-  font-synthesis: none;
-  text-rendering: optimizeLegibility;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-text-size-adjust: 100%;
-}
-
-.container {
-  margin: 0;
-  padding-top: 10vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  text-align: center;
-}
-
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: 0.75s;
-}
-
-.logo.tauri:hover {
-  filter: drop-shadow(0 0 2em #24c8db);
-}
-
-.row {
-  display: flex;
-  justify-content: center;
-}
-
-a {
-  font-weight: 500;
-  color: #646cff;
-  text-decoration: inherit;
-}
-
-a:hover {
-  color: #535bf2;
-}
-
-h1 {
-  text-align: center;
-}
-
-input,
-button {
-  border-radius: 8px;
-  border: 1px solid transparent;
-  padding: 0.6em 1.2em;
-  font-size: 1em;
-  font-weight: 500;
-  font-family: inherit;
-  color: #0f0f0f;
-  background-color: #ffffff;
-  transition: border-color 0.25s;
-  box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
-}
-
-button {
-  cursor: pointer;
-}
-
-button:hover {
-  border-color: #396cd8;
-}
-button:active {
-  border-color: #396cd8;
-  background-color: #e8e8e8;
-}
-
-input,
-button {
-  outline: none;
-}
-
-#greet-input {
-  margin-right: 5px;
-}
-
-@media (prefers-color-scheme: dark) {
-  :root {
-    color: #f6f6f6;
-    background-color: #2f2f2f;
-  }
-
-  a:hover {
-    color: #24c8db;
-  }
-
-  input,
-  button {
-    color: #ffffff;
-    background-color: #0f0f0f98;
-  }
-  button:active {
-    background-color: #0f0f0f69;
-  }
-}
-
-</style>
+<div class="flex flex-col">
+  <main id="cesiumContainer" class="h-full w-full"></main>
+  <label class="p-2">
+    <input type="text" placeholder="Enter a name to greet..." bind:value={name} />
+    <button type="submit" class="h-fit rounded-sm border border-gray-300 p-2" onclick={greet}>Greet</button>
+  </label>
+  {console.log(greetMsg)}
+  {#if greetMsg}
+    <p>{greetMsg}</p>
+  {/if}
+</div>
