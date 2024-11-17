f46a4c691d17c4ba1af0bbb0e02ddd369c0afaf2
roy.crippen4 <roy.crippen4@archarithms.com>
2024-11-17 18:05:58 -0500

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,6 @@
 
   async function greet(event: Event) {
     event.preventDefault();
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
     greetMsg = await invoke("greet", { name });
   }
 </script>
